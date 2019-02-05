4c1e8546669933ded3e1dd2e58234052972d2345
DESKTOP-0ONJAOC\Danny <dannydejonge1710@gmail.com>
2019-02-05 16:00:00 +0100

    You can now click on a pizza and it will display the pizza name, a picture and the price

--- js.js
+++ js.js
@@ -1,8 +1,12 @@
-var allPizza = ['Pizza Hawai', 'Pizza Selami', 'Pizza Calzone'];
+var allPizza = ['Pizza Hawaii', 'Pizza Salami', 'Pizza Calzone'];
+var allPrices = [5, 4, 6];
+
+var allToppings = [['Ananas', 'Ham', "Tomatensaus"],
+				['Salami', 'Paprika', 'Tomatensaus']];
 
 function startPizza(){
 
-	console.log(allPizza.length);
+	document.getElementById('pizzaInfo').style.display = 'none';
 
 	for (i = 0; i < allPizza.length; i++) { 
 
@@ -17,9 +21,33 @@ function startPizza(){
 		document.getElementById('myTdN' + i).innerHTML = i;
 
 		var tdP = document.createElement("td");
-		tdP.setAttribute("id", "myTdP" + i);
+		tdP.setAttribute("id", i);
+		
 		document.getElementById('pizzalist' + i).appendChild(tdP);
 
-		document.getElementById('myTdP' + i).innerHTML = allPizza[i];
+		document.getElementById(i).innerHTML = allPizza[i];
+
+		document.getElementById(i).onclick = function(){
+			clickPizza(this.id);
+		}
 	}
+}
+
+function clickPizza($id) {
+
+	var homePage = document.getElementById('homePage');
+	var pizzaInfo = document.getElementById('pizzaInfo');
+	var pizzaName = document.getElementById('pizzaName');
+	var pizzaPicture = document.getElementById('pizzaPicture');
+	var pizzaPrice = document.getElementById('pizzaPrice');
+
+	homePage.style.display = 'none';
+	pizzaInfo.style.display = 'block';
+
+	pizzaName.innerHTML = allPizza[$id]
+	pizzaPicture.src = allPizza[$id] + ".jpg";
+	pizzaPrice.innerHTML = '€' + allPrices[$id];
+
+	console.log(allToppings);
+	
 }
